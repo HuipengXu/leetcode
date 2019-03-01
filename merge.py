41792c2cee094607ee0174b8d7ea658e050237b5
Brycexxx <2531094755@qq.com>
2019-03-01 15:58:55 +0800

    leetcode-合并两个有序数组(添加第二种解法)

--- merge.py
+++ merge.py
@@ -10,7 +10,7 @@ https://leetcode-cn.com/explore/interview/card/top-interview-quesitons-in-2018/2
 from typing import List
 
 
-def merge(nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+def merge0(nums1: List[int], m: int, nums2: List[int], n: int) -> None:
     """
     Do not return anything, modify nums1 in-place instead.
     """
@@ -26,3 +26,31 @@ def merge(nums1: List[int], m: int, nums2: List[int], n: int) -> None:
                 nums2[k], nums2[k + 1] = nums2[k + 1], nums2[k]
                 k += 1
     nums1[m:] = nums2[:]
+
+
+def merge1(nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+    """
+    Do not return anything, modify nums1 in-place instead.
+    """
+    if len(nums1) == 0 or n == 0: return None
+    i = m - 1
+    j = n - 1
+    k = m + n - 1
+    while i >= 0 and j >= 0:
+        if nums1[i] >= nums2[j]:
+            nums1[k] = nums1[i]
+            i -= 1
+        else:
+            nums1[k] = nums2[j]
+            j -= 1
+        k -= 1
+    if i == -1:
+        while j >= 0:
+            nums1[k] = nums2[j]
+            k -= 1
+            j -= 1
+    else:
+        while i >= 0:
+            nums1[k] = nums1[i]
+            k -= 1
+            i -= 1
