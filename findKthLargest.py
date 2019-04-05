6da8dadf9a8dc5fe6cda220879937c6cddc8152d
Brycexxx <2531094755@qq.com>
2019-04-05 10:18:55 +0800

    leetcode-215 (优化查找效率)

--- findKthLargest.py
+++ findKthLargest.py
@@ -35,10 +35,34 @@ class Solution:
         self.quick_sort(nums, k)
         return nums[len(nums) - k]
 
+    def super_findKthLargest(self, nums: List[int], k: int) -> int:
+        def partition(nums: List[int], start: int, end: int) -> int:
+            pivot_idx = random.randint(start, end - 1)
+            nums[end - 1], nums[pivot_idx] = nums[pivot_idx], nums[end - 1]
+            j = start
+            for i in range(start, end):
+                if nums[i] >= nums[end - 1]:
+                    nums[i], nums[j] = nums[j], nums[i]
+                    j += 1
+            return j - 1
+
+        start, end = 0, len(nums)
+        while True:
+            mid = partition(nums, start, end)
+            if k - 1 == mid:
+                return nums[mid]
+            elif k - 1 < mid:
+                start = start
+                end = mid
+            else:
+                start = mid + 1
+                end = end
+
+
 
 if __name__ == "__main__":
     nums = [3, 2, 1, 5, 6, 4]
-    k = 1
+    k = 4
     s = Solution()
     print(sorted(nums))
-    print(s.findKthLargest(nums, k))
+    print(s.super_findKthLargest(nums, k))
