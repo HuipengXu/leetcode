ae64d2a352b06a3109fc05db0b9e3cff7d44ccc2
Brycexxx <2531094755@qq.com>
2019-04-10 12:55:21 +0800

    leetcode-237

---
+++ deleteNode.py
@@ -0,0 +1,19 @@
+# @Time    : 2019/4/10 12:53
+# @Author  : Xu Huipeng
+# @Blog    : https://brycexxx.github.io/
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    def deleteNode(self, node):
+        """
+        :type node: ListNode
+        :rtype: void Do not return anything, modify node in-place instead.
+        """
+        node.val = node.next.val
+        node.next = node.next.next
