1d452ee74f6fbe7c669c8ec2a70d638737ada885
Brycexxx <2531094755@qq.com>
2019-04-14 15:51:39 +0800

    leetcode-324 (添加 O(n) 解法)

--- wiggleSort.py
+++ wiggleSort.py
@@ -1,8 +1,7 @@
 # @Time    : 2019/4/14 9:14
 # @Author  : Xu Huipeng
 # @Blog    : https://brycexxx.github.io/
-from typing import List
-from random import randint
+from typing import List, Optional
 
 
 class Solution:
@@ -56,10 +55,58 @@ class Solution:
         ret = wiggle(nums)
         nums[:] = ret[:]
 
+    def super_wiggle_sort(self, nums: List[int]) -> None:
+        def findKthLargest(nums: List[int], k: int) -> Optional[int]:
+            if k > len(nums) or k <= 0:
+                return None
+
+            def partition(nums: list, p: int, r: int):
+                pivot = nums[r - 1]
+                i = p
+                for j in range(p, r):
+                    if nums[j] >= pivot:
+                        nums[j], nums[i] = nums[i], nums[j]
+                        i += 1
+                return i
+
+            p = 0
+            r = len(nums)
+            while True:
+                q = partition(nums, p, r)
+                if q == k:
+                    return
+                elif q > k:
+                    p = 0
+                    r = q - 1
+                else:
+                    p = q
+                    r = len(nums)
+
+        mid = (len(nums) + 1) // 2
+        findKthLargest(nums, mid)
+        median = nums[mid - 1]
+        re_index = lambda x: (1 + 2 * x) % (len(nums) | 1)
+        # 这块索引的变换真的搞不太懂
+        i = j = 0
+        k = len(nums) - 1
+        while j <= k:
+            # 大于中位数的放到奇数位
+            if nums[re_index(j)] > median:
+                nums[re_index(i)], nums[re_index(j)] = nums[re_index(j)], nums[re_index(i)]
+                i += 1
+                j += 1
+            # 小于中位数的放到偶数位
+            elif nums[re_index(j)] < median:
+                nums[re_index(j)], nums[re_index(k)] = nums[re_index(k)], nums[re_index(j)]
+                k -= 1
+            else:
+                j += 1
+
 
 if __name__ == "__main__":
     s = Solution()
-    # nums = [2, 4, 4, 6, 7, 7, 8]
-    nums = [4, 5, 5, 6]
-    s.wiggleSort(nums)
+    nums = [2, 4, 4, 6, 7, 7, 8]
+    # nums = [4, 5, 5, 6]
+    # s.wiggleSort(nums)
+    s.super_wiggle_sort(nums)
     print(nums)
