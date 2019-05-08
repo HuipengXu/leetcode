ce5724b37c29393d1a16bd1f7334c1788ff86144
Brycexxx <2531094755@qq.com>
2019-05-08 16:13:03 +0800

    leetcode-4 (看了官方答案的解析)

--- findMedianSortedArrays.py
+++ findMedianSortedArrays.py
@@ -6,6 +6,7 @@
 4. 寻找两个有序数组的中位数
 时间复杂度为 O(m+n)，未达到题目要求 O(log(m+n))
 """
+from typing import List
 
 
 class Solution:
@@ -33,3 +34,56 @@ class Solution:
             return new_nums[int((l - 1) / 2)]
         else:
             return (new_nums[int(l / 2)] + new_nums[int(l / 2) - 1]) / 2
+
+    def findMedianSortedArrays1(self, nums1: List[int], nums2: List[int]) -> float:
+        i = j = k = 0
+        total_length = len(nums1) + len(nums2)
+        tmp = [0] * (total_length // 2 + 1)
+        flag = True if total_length % 2 else False
+        while k <= total_length // 2 and i < len(nums1) and j < len(nums2):
+            if nums1[i] <= nums2[j]:
+                tmp[k] = nums1[i]
+                i += 1
+            else:
+                tmp[k] = nums2[j]
+                j += 1
+            k += 1
+        while k <= total_length // 2:
+            if i < len(nums1):
+                tmp[k] = nums1[i]
+                k += 1
+                i += 1
+            else:
+                tmp[k] = nums2[j]
+                k += 1
+                j += 1
+        return tmp[-1] if flag else (tmp[-1] + tmp[-2]) / 2
+
+    # 参考答案
+    def findMedianSortedArrays2(self, nums1: List[int], nums2: List[int]) -> float:
+        shorter_nums, longer_nums = nums1, nums2 if len(nums1) <= len(nums2) else (nums2, nums1)
+        s_len, l_len = len(shorter_nums), len(longer_nums)
+        s, t = 0, s_len
+        while s <= t:
+            i = s + ((t - s) >> 1)
+            j = (s_len + l_len + 1) // 2 - i
+            if i < s_len and longer_nums[j - 1] > shorter_nums[i]:
+                s = i + 1
+            elif i > 0 and shorter_nums[i - 1] > longer_nums[j]:
+                t = i
+            else:
+                if i == 0:
+                    max_of_left = longer_nums[j - 1]
+                elif j == 0:
+                    max_of_left = shorter_nums[i - 1]
+                else:
+                    max_of_left = max(longer_nums[j - 1], shorter_nums[i - 1])
+                if (s_len + l_len) % 2:
+                    return max_of_left
+                if i == s_len:
+                    min_of_right = longer_nums[j]
+                elif j == l_len:
+                    min_of_right = shorter_nums[i]
+                else:
+                    min_of_right = min(longer_nums[j], shorter_nums[i])
+                return (min_of_right + max_of_left) / 2.0
