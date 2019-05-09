e6a69f90b96a1162c8c4d68c782cf3fa19d6184c
Brycexxx <2531094755@qq.com>
2019-05-09 19:33:52 +0800

    leetcode-14 (一些不一样的思路)

--- longestCommonPrefix.py
+++ longestCommonPrefix.py
@@ -1,3 +1,5 @@
+from typing import List
+
 def longestCommonPrefix(strs):
     """
     :type strs: List[str]
@@ -17,3 +19,55 @@ def longestCommonPrefix(strs):
             break
     return strs[0][:index]
 
+
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        if len(strs) == 0: return ''
+        i = 0
+        flag = False
+        while True:
+            if i < len(strs[0]):
+                tmp = strs[0][i]
+            else:
+                break
+            for word in strs[1:]:
+                if i < len(word) and word[i] == tmp:
+                    continue
+                flag = True
+                break
+            if flag:
+                break
+            else:
+                i += 1
+        return strs[0][:i]
+
+    # 骚操作
+    def longestCommonPrefix1(self, strs: List[str]) -> str:
+        if len(strs) == 0: return ''
+        s1 = max(strs)
+        s2 = min(strs)
+        for i, c in enumerate(s2):
+            if c != s1[i]:
+                return s2[:i]
+        return s2
+
+    def longestCommonPrefix2(self, strs: List[str]) -> str:
+        if len(strs) == 0: return ''
+        common_prefix = strs[0]
+        for s in strs[1:]:
+            i = 0
+            while i < len(common_prefix) and i < len(s):
+                if s[i] == common_prefix[i]:
+                    i += 1
+                    continue
+                break
+            if common_prefix == '': return ''
+            common_prefix = common_prefix[:i]
+        return common_prefix
+
+
+if __name__ == '__main__':
+    ss = ["flower", "flow", "flight"]
+    s = Solution()
+    print(s.longestCommonPrefix1(ss))
+    print(s.longestCommonPrefix2(ss))
