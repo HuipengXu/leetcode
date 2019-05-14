06a9daa35aa23d36c9bb5c850a5545bb96204c92
Brycexxx <2531094755@qq.com>
2019-05-14 11:14:08 +0800

    leetcode-21(以前写的真是不堪入目:joy:)

--- mergeTwoLists.py
+++ mergeTwoLists.py
@@ -1,9 +1,9 @@
 # 21. 合并两个有序链表
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
 
 class Solution:
     def mergeTwoLists(self, l1, l2):
@@ -44,3 +44,30 @@ class Solution:
             list_node = list_node.next
 
         return lst
+
+    def mergeTwoLists1(self, l1: ListNode, l2: ListNode) -> ListNode:
+        if not l1: return l2
+        if not l2: return l1
+        if l1.val < l2.val:
+            l1.next = self.mergeTwoLists(l1.next, l2)
+            return l1
+        else:
+            l2.next = self.mergeTwoLists(l1, l2.next)
+            return l2
+
+    def mergeTwoLists2(self, l1: ListNode, l2: ListNode) -> ListNode:
+        if not l1: return l2
+        if not l2: return l1
+        ret = ListNode(0)
+        node = ret
+        while l1 and l2:
+            if l1.val < l2.val:
+                node.next = l1
+                l1 = l1.next
+            else:
+                node.next = l2
+                l2 = l2.next
+            node = node.next
+        if l1: node.next = l1
+        if l2: node.next = l2
+        return ret.next
