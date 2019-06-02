ec68fd2cc04e9ff6abc9a4a36019bc99a2cac55c
Brycexxx <2531094755@qq.com>
2019-06-02 21:41:50 +0800

    leetcode-105

---
+++ buildTree.py
@@ -0,0 +1,23 @@
+# @Time    : 2019/6/2 20:34
+# @Author  : Xu Huipeng
+# @Blog    : https://brycexxx.github.io/
+
+from typing import List, Optional
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
+        if not preorder: return
+        root = TreeNode(preorder[0])
+        mid = inorder.index(root.val)
+        root.left = self.buildTree(preorder[1:mid + 1], inorder[:mid])
+        root.right = self.buildTree(preorder[mid + 1:], inorder[mid + 1:])
+        return root
