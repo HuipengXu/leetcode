dd435a8f4d2913d5d926c417a91b01371a08a642
Brycexxx <2531094755@qq.com>
2019-06-29 08:49:33 +0800

    leetcode-167

---
+++ twoSumTwo.py
@@ -0,0 +1,19 @@
+# @Time    : 2019/6/29 8:37
+# @Author  : Xu Huipeng
+# @Blog    : https://brycexxx.github.io/
+
+from typing import List
+
+
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        low, high = 0, len(numbers) - 1
+        while low < high:
+            tmp = numbers[low] + numbers[high]
+            if tmp > target:
+                high -= 1
+            elif tmp < target:
+                low += 1
+            else:
+                return [low + 1, high + 1]
+        return [-1, -1]
